9d1555bef23d9aab3f0163f7a72b77588ca721c0
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2020-05-15 16:08:14 -0300

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,6 +1,7 @@
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
 cd /mnt/tmp
 wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
 chmod -x install.sh
