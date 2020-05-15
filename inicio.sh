efa20829a5af56500706c3be591412ed66411915
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-15 15:30:06 -0300

    Erros iniciais corrigidos, v.2.0

--- inicio.sh
+++ inicio.sh
@@ -1,5 +1,5 @@
 vim /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget
+pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 cd /tmp
--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
 hwclock --systohc
+
 echo en_US.UTF-8 UTF-8 >> vim /etc/locale.gen
-locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+locale-gen
+
 echo -e "Digite seu nome do pc\nObs: sera o nome do pc na rede"
 read nome_usuario
 echo $nome_usuario > /etc/hostname
@@ -11,6 +15,7 @@ echo -e "::1\t\tlocalhost.localdomain\tlocalhost" >> /etc/hosts
 echo -e "127.0.1.1\t$nome_usuario.localdomain\t$nome_usuario" >> /etc/hosts
 echo "Agora vamos colocar a senha do root"
 passwd
+
 # trecho adaptado do codigo do terminalroot
 echo "Agora vamos criar um usuario e incluir no grupo sudo!" 
 echo "Informe seu nome de usuario" 
@@ -27,19 +32,21 @@ echo "exec i3" > /home/${muser}/.xinitrc
 #reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Grub legacy
-#pacman -S grub amd-ucode
-#grub-install --target=i386-pc --recheck /dev/sda
-#mkdir /boot/grub/locale
-#touch /boot/grub/locale/en.mo
-#cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-
-#grub UEFI
-pacman -S grub-efi-x86_64 efibootmgr amd-ucode
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+pacman -S grub amd-ucode
+grub-install --target=i386-pc --recheck /dev/sda
 mkdir /boot/grub/locale
 touch /boot/grub/locale/en.mo
-cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
+#grub UEFI
+#pacman -S grub-efi-x86_64 efibootmgr amd-ucode
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+#mkdir /boot/grub/locale
+#touch /boot/grub/locale/en.mo
+#cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
+
+cd
+mkdir Documents Music Downloads Pictures Video
