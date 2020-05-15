25642b0258e5bb1bb04f0f635e3580f4e45b30c1
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2020-05-15 15:46:18 -0300

    o scipt nao faz nada dps do arch-chroot /mnt

--- inicio.sh
+++ inicio.sh
@@ -1,7 +1,7 @@
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-cd /tmp
+cd /mnt/tmp
 wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
 chmod -x install.sh
+arch-chroot /mnt
