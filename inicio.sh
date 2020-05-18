a4b7abf8abdd746f92edc57ee608235af0b37524
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-18 19:12:27 -0300

    arrumei o wget para continuacao do script

--- inicio.sh
+++ inicio.sh
@@ -2,7 +2,7 @@ vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-cd /mnt/tmp
+cd /mnt
 wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
 chmod -x install.sh
 arch-chroot /mnt
