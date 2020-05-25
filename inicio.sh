571b80c85ed4b0a8d916f83a4c86282b7058f34d
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-25 18:26:36 -0300

    colocando o spell pt-br

--- inicio.sh
+++ inicio.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-cd /mnt
+cd /mnt || exit
 wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
 chmod -x install.sh
 arch-chroot /mnt
