05de4434a3d1d6a15135852408224905b63167e9
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-25 18:32:13 -0300

    teste para maquina virtual

--- inicio.sh
+++ inicio.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Crie o boot, swap, e /, nessa ordem"
+sleep 5
+
+cfdisk -L
+
+mkfs.ext /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mkfs.ext /dev/sda3
+
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
