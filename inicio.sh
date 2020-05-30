e79ab46f99345886c2a423737ca261d1d46f5561
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-29 22:15:30 -0300

    Faltou os mounts

--- inicio.sh
+++ inicio.sh
@@ -6,15 +6,19 @@ sleep 5
 cfdisk -L
 
 mkfs.ext /dev/sda1
+mkfs.ext /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
-mkfs.ext /dev/sda3
+
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-cd /mnt || exit
-wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
-chmod -x install.sh
+#cd /mnt || exit
+#wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
+#chmod -x install.sh
 arch-chroot /mnt
