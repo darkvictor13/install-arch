3fad3dd36a5381b73567aa3a815045b48d22b65d
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-05-29 22:46:50 -0300

    Mudei echo, coloquei &&

--- inicio.sh
+++ inicio.sh
@@ -5,26 +5,16 @@ sleep 5
 
 cfdisk -L
 
-mkfs.ext /dev/sda1
-echo "Fiz fs do boot"
-mkfs.ext /dev/sda3
-echo "Fiz fs do /"
-mkswap /dev/sda2
-swapon /dev/sda2
-echo "Fiz a swap"
-
-mount /dev/sda3 /mnt
-echo "Montei o /"
-mkdir /mnt/boot
-echo "criei a pasta boot"
-mount /dev/sda1 /mnt/boot
-echo "montei o boot"
+mkfs.ext /dev/sda3 && mount /dev/sda3 /mnt
+mkfs.ext /dev/sda1 && mkdir /mnt/boot && mount /dev/sda1 /mnt/boot 
+mkswap /dev/sda2 && swapon /dev/sda2
 
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager wget git
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 echo "fiz fstab"
+cat /mnt/etc/fstab
 
 #cd /mnt || exit
 #wget https://raw.githubusercontent.com/darkvictor13/install-arch/master/install.sh
